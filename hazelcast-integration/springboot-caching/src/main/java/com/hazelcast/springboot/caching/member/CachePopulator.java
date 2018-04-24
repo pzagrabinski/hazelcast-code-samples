b10c7cf8fe889bf345a5818150dc0e29ea5dd0a4
Tomasz Lelek <tomasz.lelek@allegro.pl>
2018-04-24 19:15:28 +0200

    pre-populating success

--- hazelcast-integration/springboot-caching/src/main/java/com/hazelcast/springboot/caching/member/CachePopulator.java
+++ hazelcast-integration/springboot-caching/src/main/java/com/hazelcast/springboot/caching/member/CachePopulator.java
@@ -1,5 +1,9 @@
 package com.hazelcast.springboot.caching.member;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.springboot.caching.serialization.Person;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +13,14 @@ public class CachePopulator {
 
     @PostConstruct
     private void populateCache() {
+        HazelcastInstance client = HazelcastClient.newHazelcastClient();
+
+        // Create a Hazelcast backed map
+        IMap<Integer, String> map = client.getMap("training");
+
+        map.put(1, "Tom");
+        map.put(2, "Jim");
+
 
     }
 }
